4c390d60bb0eddf89e8e999b39fb7ccaeb1fe76c
Aline Maia (✿◡‿◡) <alimaaia.dev@gmail.com>
2025-08-09 11:22:13 -0300

    Adição do bootstrap no menu principal

--- login/login.php
+++ login/login.php
@@ -1,4 +1,5 @@
 <?php 
+    session_start();
     include_once('../conexao.php');
 
     $email = $_POST['email'];
@@ -8,9 +9,12 @@
     $resultado = mysqli_query($conexao,$sql); //armazena o resultado da consulta anterior
 
     if (mysqli_num_rows($resultado) > 0){
-            echo "Login feito com sucesso! Bem-vindo!";
+            $_SESSION['email'] = $email;
+            $_SESSION['senha'] = $senha;
+            header("Location: ../menu/index.php");
         }
     else{
-        echo "Email ou senha incorretos!";
+        echo '<script>alert("Email ou senha incorretos. Tente novamente!");window.location.href = "index.html";</script>';
+            exit;
     }
 ?>
---
+++ menu/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="bootstrap/css/bootstrap.css" rel="stylesheet">
+    <script src="bootstrap/js/bootstrap.js" async></script>
+    <link href="style.css" rel="stylesheet">
+</head>
+<body>
+    <header>
+        <nav class="navbar fixed-top navbar-expand-lg bg-body-transparent">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#inicio"><img src="imagens/logo.png" id="logop" alt="Logo"></a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav" id="tudo">
+              <li class="nav-item">
+                <a class="nav-link active" id="comeco" href="#">Início</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Sobre mim</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Hard e Soft skills</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Meus projetos</a>
+              </li>
+            </ul>
+              <a class="nav-link" href="#"><button type="submit" id="botao">Contatos</button></a>
+          </div>
+        </div>
+        </nav>
+    </header>
+</body>
+</html>
